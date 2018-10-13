0c12a2ec725b9892e6bec3eff8d52ffb1382025b
Or Fleisher <fleisher.or@gmail.com>
2018-10-12 22:26:48 -0400

    Initial Commit

---
+++ DepthReader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DepthReader
+//
+//  Created by Or Fleisher on 10/12/18.
+//  Copyright © 2018 Or Fleisher. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DepthReader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DepthReader
+//
+//  Created by Or Fleisher on 10/12/18.
+//  Copyright © 2018 Or Fleisher. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
